014096e520523f98fef9451f28b174f6db6050c8
MJ Vermaak <vermaakmj2004@gmail.com>
2026-09-02 15:48:38 +0200

    Final touches for milestone 2
    Fixed final errors and built final backend stack successfully

--- GrandmastersHub/GrandmastersHub.Infrastructure/Data/Configurations/ReviewConfiguration.cs
+++ GrandmastersHub/GrandmastersHub.Infrastructure/Data/Configurations/ReviewConfiguration.cs
@@ -1,22 +1,43 @@
 using GrandmastersHub.Domain.Entities;
+
 using Microsoft.EntityFrameworkCore;
+
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
 
 namespace GrandmastersHub.Infrastructure.Data.Configurations;
 
+
+
 public class ReviewConfiguration : IEntityTypeConfiguration<Review>
+
 {
+
     public void Configure(EntityTypeBuilder<Review> builder)
+
     {
+
         builder.HasKey(r => r.ReviewId);
 
+
+
         builder.Property(r => r.Rating)
+
             .IsRequired();
 
+
+
         builder.Property(r => r.Comment)
+
             .HasMaxLength(1000);
 
+
+
         builder.HasIndex(r => new { r.UserId, r.ProductId })
+
             .IsUnique();
+
     }
+
 }
