2f0d9aff4c90e4fbb13c6aa1c30f6c48bdc39bae
Kimberly90 <kimberlykaronga2@gmail.com>
2026-09-09 11:59:22 +0200

    Implement API validation and sanitization

---
+++ GrandmastersHub/GrandmastersHub.Application/Helpers/InputSanitizer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Text.RegularExpressions;
+
+namespace GrandmastersHub.Application.Helpers;
+
+public static class InputSanitizer
+{
+    public static string Clean(string input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+            return string.Empty;
+
+        input = input.Trim();
+
+        input = Regex.Replace(input, @"\s+", " ");
+
+        return input;
+    }
+}
