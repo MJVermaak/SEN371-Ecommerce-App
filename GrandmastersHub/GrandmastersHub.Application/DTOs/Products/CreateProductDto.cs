3e1fab46d1af5ecd06afe6823933fb23b797369a
Kimberly90 <kimberlykaronga2@gmail.com>
2026-09-09 11:59:22 +0200

    Implement API validation and sanitization

--- GrandmastersHub/GrandmastersHub.Application/DTOs/Products/CreateProductDto.cs
+++ GrandmastersHub/GrandmastersHub.Application/DTOs/Products/CreateProductDto.cs
@@ -2,9 +2,29 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace GrandmastersHub.Application.DTOs.Products
+using System.ComponentModel.DataAnnotations;
+
+namespace GrandmastersHub.Application.DTOs
 {
-    internal class CreateProductDto
+    public class CreateProductDto
     {
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(500)]
+        public string Description { get; set; } = string.Empty;
+
+        [Range(0.01, 100000)]
+        public decimal Price { get; set; }
+
+        [Range(0, 10000)]
+        public int StockQuantity { get; set; }
+
+        public int CategoryId { get; set; }
+
+        [Url]
+        public string ImageUrl { get; set; } = string.Empty;
     }
 }
